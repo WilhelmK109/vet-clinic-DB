6c81d6aef69265694e1f38bf00f71a9e1aea89b1
WilhelmK109 <kamulungaw@gmail.com>
2023-07-12 16:41:46 +0200

    Populate owners table with some data

--- data.sql
+++ data.sql
@@ -37,3 +37,11 @@ VALUES ('Charmander', '2020-02-08', 0, false, -11),
   ('Boarmon', '2005-06-07', 7, true, 20.4), 
   ('Blossom', '1998-10-13', 3, true, 17), 
   ('Ditto', '2022-05-14', 4, true, 22);
+
+INSERT INTO owners (full_name, age) 
+VALUES ('Sam Smith', 34),
+	('Jennifer Orwell', 19),
+	('Bob', 45),
+	('Melody Pond', 77),
+	('Dean Winchester', 14),
+	('Jodie Whittaker', 38);
